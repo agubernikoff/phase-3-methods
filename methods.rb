36aa0c79187ebab210a31f9d58b1328fdc82129e
alexander gubernikoff <agubernikoff@gmail.com>
2022-03-13 18:21:30 -0400

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,17 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"end
+
+def greet(programmer)
+    puts "Hello, #{programmer}!"end
+
+def greet_with_default(programmer="programmer")
+    puts "Hello, #{programmer}!"end
+
+def add(num1,num2)
+    num1 +num2 end
+
+def halve(int)
+    if int.class != Integer 
+        return nil 
+    end
+    int/2 end
